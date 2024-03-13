a1be5091f472b284796c08db8f180ff427d8c7f4
Mariam Meerza <mariam@netfotech.in>
2024-03-13 12:57:58 +0530

    second

--- lib/Maths_Screens/basicoperations.dart
+++ lib/Maths_Screens/basicoperations.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 
 class BasicOperations extends StatefulWidget {
-  const BasicOperations({super.key});
+  const BasicOperations({Key? key}) : super(key: key);
 
   @override
   State<BasicOperations> createState() => _BasicOperationsState();
@@ -13,104 +13,100 @@ class _BasicOperationsState extends State<BasicOperations> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     var screenHeight = size.height;
-    var screenWidth = size.width;
 
     return Scaffold(
-        body: Stack(children: <Widget>[
-      ClipRRect(
-        borderRadius: const BorderRadius.only(
-          bottomLeft: Radius.circular(25),
-          bottomRight: Radius.circular(25),
-        ),
-        child: Container(
-          height: screenHeight * 0.28,
-          decoration: const BoxDecoration(
-            color: Color(0XFFCDE9C8),
-          ),
-        ),
-      ),
-      Padding(
-        padding: EdgeInsets.only(
-          left: MediaQuery.of(context).size.width * 0.15,
-          top: MediaQuery.of(context).size.height * 0.035,
-        ),
-        child: const Text(
-          'Back',
-          style: TextStyle(
-            fontFamily: 'OnboardFont2',
-            fontSize: 27.0,
-            color: Color(0xff494949),
-          ),
-        ),
-      ),
-      Positioned(
-        top: screenHeight * 0.03,
-        left: 10,
-        child: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          iconSize: 33,
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      SafeArea(
-        child: Padding(
-            padding: EdgeInsets.only(
-              top: screenHeight * 0.23,
-              left: screenHeight * 0.05,
+      body: Stack(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(25),
+              bottomRight: Radius.circular(25),
             ),
-            child: Column(
-              children: [
-                Container(
-
-                  width: screenHeight * 0.38,
-                  height: screenHeight * 0.05,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(width: 2, color: Color(0XFFCDE9C8)),
-                    borderRadius: BorderRadius.circular(40),
+            child: Container(
+              height: screenHeight * 0.28,
+              decoration: const BoxDecoration(
+                color: Color(0XFFCDE9C8),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: MediaQuery.of(context).size.width * 0.15,
+              top: MediaQuery.of(context).size.height * 0.035,
+            ),
+            child: const Text(
+              'Back',
+              style: TextStyle(
+                fontFamily: 'OnboardFont2',
+                fontSize: 27.0,
+                color: Color(0xff494949),
+              ),
+            ),
+          ),
+          Positioned(
+            top: screenHeight * 0.03,
+            left: 10,
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              iconSize: 33,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: screenHeight * 0.23,
+                left: screenHeight * 0.05,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    width: screenHeight * 0.38,
+                    height: screenHeight * 0.1,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(width: 2, color: Color(0XFFCDE9C8)),
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: CircularCountDownTimer(
+                      duration: 20,
+                      initialDuration: 0,
+                      controller: CountDownController(),
+                      width: 50,
+                      height: 50,
+                      ringColor: Colors.grey,
+                      ringGradient: null,
+                      fillColor: Colors.blue,
+                      fillGradient: null,
+                      backgroundColor: Colors.white,
+                      backgroundGradient: null,
+                      strokeWidth: 10.0,
+                      strokeCap: StrokeCap.round,
+                      textStyle: const TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textFormat: CountdownTextFormat.S,
+                      isReverse: true,
+                      isReverseAnimation: false,
+                      isTimerTextShown: true,
+                      autoStart: true,
+                      onStart: () {
+                        print('Countdown Started');
+                      },
+                      onComplete: () {
+                        print('Countdown Ended');
+                      },
+                    ),
                   ),
-                  //                 Padding(
-                  // padding: EdgeInsets.only(
-                  //   left: screenHeight * 0.33,
-                  //   bottom: screenHeight * 0.5,
-                  // ),
-                  child: CircularCountDownTimer(
-                          duration: 20,
-                          initialDuration: 0,
-                          controller: CountDownController(),
-                          width: 50,
-                          height: 50,
-                          ringColor: Colors.grey,
-                          ringGradient: null,
-                          fillColor: Colors.blue,
-                          fillGradient: null,
-                          backgroundColor: Colors.white,
-                          backgroundGradient: null,
-                          strokeWidth: 10.0,
-                          strokeCap: StrokeCap.round,
-                          textStyle: const TextStyle(
-                              fontSize: 16.0,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold),
-                          textFormat: CountdownTextFormat.S,
-                          isReverse: true,
-                          isReverseAnimation: false,
-                          isTimerTextShown: true,
-                          autoStart: true,
-                          onStart: () {
-                            print('Countdown Started');
-                          },
-                          onComplete: () {
-                            print('Countdown Ended');
-                          },
-                        ),
-                ),
-                  child: Container(
-                    padding: EdgeInsets.only(
-                      top: screenHeight * 0.01,
-                      left: screenHeight * 0.02,
+                  Container(
+                    padding: const EdgeInsets.only(
+                      top: 10,
+                      left: 10,
                     ),
                     child: RichText(
                       text: const TextSpan(
@@ -135,47 +131,12 @@ class _BasicOperationsState extends State<BasicOperations> {
                       ),
                     ),
                   ),
-                ),
-                
-                // Padding(
-                //   padding: EdgeInsets.only(
-                //     left: screenHeight * 0.33,
-                //     bottom: screenHeight * 0.5,
-                //   ),
-                //   child: CircularCountDownTimer(
-                //           duration: 20,
-                //           initialDuration: 0,
-                //           controller: CountDownController(),
-                //           width: 50,
-                //           height: 50,
-                //           ringColor: Colors.grey,
-                //           ringGradient: null,
-                //           fillColor: Colors.blue,
-                //           fillGradient: null,
-                //           backgroundColor: Colors.white,
-                //           backgroundGradient: null,
-                //           strokeWidth: 10.0,
-                //           strokeCap: StrokeCap.round,
-                //           textStyle: const TextStyle(
-                //               fontSize: 16.0,
-                //               color: Colors.black,
-                //               fontWeight: FontWeight.bold),
-                //           textFormat: CountdownTextFormat.S,
-                //           isReverse: true,
-                //           isReverseAnimation: false,
-                //           isTimerTextShown: true,
-                //           autoStart: true,
-                //           onStart: () {
-                //             print('Countdown Started');
-                //           },
-                //           onComplete: () {
-                //             print('Countdown Ended');
-                //           },
-                //         ),
-                // ),
-              ],
-            )),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
-    ]));
+    );
   }
 }
